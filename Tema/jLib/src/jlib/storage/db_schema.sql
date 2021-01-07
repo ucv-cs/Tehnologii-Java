d28c3cd3838966e219e289419f84216f2a85092e
Alin <alin.clincea@outlook.com>
2021-01-07 16:55:24 +0200

    * minor changes to the database

--- Tema/jLib/src/jlib/storage/db_schema.sql
+++ Tema/jLib/src/jlib/storage/db_schema.sql
@@ -1,16 +1,4 @@
 BEGIN TRANSACTION;
-DROP TABLE IF EXISTS "borrows";
-CREATE TABLE IF NOT EXISTS "borrows" (
-	"book_id"	INTEGER NOT NULL,
-	"reader_id"	INTEGER NOT NULL,
-	"date_borrowed"	TEXT NOT NULL,
-	"date_to_return"	TEXT NOT NULL,
-	"date_returned"	TEXT NOT NULL,
-	"librarian_id"	INTEGER NOT NULL,
-	FOREIGN KEY("reader_id") REFERENCES "readers"("id"),
-	FOREIGN KEY("book_id") REFERENCES "books"("id"),
-	FOREIGN KEY("librarian_id") REFERENCES "librarians"("id")
-);
 DROP TABLE IF EXISTS "books";
 CREATE TABLE IF NOT EXISTS "books" (
 	"id"	INTEGER NOT NULL UNIQUE,
@@ -25,15 +13,6 @@ CREATE TABLE IF NOT EXISTS "books" (
 	"status"	TEXT,
 	PRIMARY KEY("id" AUTOINCREMENT)
 );
-DROP TABLE IF EXISTS "readers";
-CREATE TABLE IF NOT EXISTS "readers" (
-	"id"	INTEGER NOT NULL UNIQUE,
-	"name"	TEXT NOT NULL,
-	"address"	TEXT,
-	"photo"	TEXT,
-	"status"	TEXT NOT NULL,
-	PRIMARY KEY("id" AUTOINCREMENT)
-);
 DROP TABLE IF EXISTS "librarians";
 CREATE TABLE IF NOT EXISTS "librarians" (
 	"id"	INTEGER NOT NULL UNIQUE,
@@ -44,5 +23,29 @@ CREATE TABLE IF NOT EXISTS "librarians" (
 	"photo"	TEXT,
 	"status"	TEXT,
 	PRIMARY KEY("id" AUTOINCREMENT)
+);
+DROP TABLE IF EXISTS "readers";
+CREATE TABLE IF NOT EXISTS "readers" (
+	"id"	INTEGER NOT NULL UNIQUE,
+	"name"	TEXT NOT NULL,
+	"address"	TEXT,
+	"photo"	TEXT,
+	"status"	TEXT,
+	PRIMARY KEY("id" AUTOINCREMENT)
+);
+DROP TABLE IF EXISTS "borrows";
+CREATE TABLE IF NOT EXISTS "borrows" (
+	"id"	INTEGER NOT NULL UNIQUE,
+	"book_id"	INTEGER NOT NULL,
+	"reader_id"	INTEGER NOT NULL,
+	"date_borrowed"	TEXT NOT NULL,
+	"date_to_return"	TEXT NOT NULL,
+	"date_returned"	TEXT,
+	"librarian_id"	INTEGER,
+	"active"	INTEGER NOT NULL DEFAULT 1,
+	PRIMARY KEY("id" AUTOINCREMENT),
+	FOREIGN KEY("book_id") REFERENCES "books"("id"),
+	FOREIGN KEY("reader_id") REFERENCES "readers"("id"),
+	FOREIGN KEY("librarian_id") REFERENCES "librarians"("id")
 );
 COMMIT;
